0e853e7bbe2c771908d0db2a2dbbd47f49f87780
karenhov <karenhov034044@gmail.com>
2023-07-17 22:12:32 +0400

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
-let number=4;
-console.log(number+1);
+//let a;
+//let b;
+//    answer=a+b;
+//if (a > b) {
+//    console.log(3,2)
+//} else {
+//    console.log(answer)
+//} 
+//for (let i=1;i<8;i++) {
+//    let a=i;
+//       b=a*i;
+//    console.log(b-a)
+//}
+function use(x,y) {
+    let a=x*2+y*2
+    console.log(a)
+}
+use(0,0);
