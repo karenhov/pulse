cd773be4ab17bbd1337ae896923e5989e77fc439
karenhov <karenhov034044@gmail.com>
2023-07-19 22:34:21 +0400

    commit_changeis

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"</button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
         });
+  }); */
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    speed: 1200,
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+  document.querySelector('.prev').addEventListener('click',  function () {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click',  function () {
+    slider.goTo('next');
   });
