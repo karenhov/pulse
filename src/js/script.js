03e4631938b41f2730776dbda40ecbc732d27fd6
karenhov <karenhov034044@gmail.com>
2023-07-22 19:30:15 +0400

    commit_tab

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,4 @@
-/* $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
-        });
-  }); */
+
   const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -19,4 +12,18 @@
   });
   document.querySelector('.next').addEventListener('click',  function () {
     slider.goTo('next');
+  });
+
+  $(document).ready(function(){
+    /* $('.carousel__inner').slick({
+        speed: 1200,
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+        }); */
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+          $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
   });
